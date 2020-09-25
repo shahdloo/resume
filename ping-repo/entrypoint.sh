9944620ed8543d5cd7ccdf7bbd331ade8cc600d2
shahdloo <shahdloo@gmail.com>
2020-09-25 11:06:37 +0100

    dummy update

--- ping-repo/entrypoint.sh
+++ ping-repo/entrypoint.sh
@@ -3,7 +3,7 @@
 main() {
   curl -XPOST -H "Accept: application/vnd.github.everest-preview+json" \
   -H "Content-Type: application/json" \
-  -H "Authorization: token ${SECRET_CV}" \
+  -H "Authorization: token ${GITHUB_TOKEN}" \
   "https://api.github.com/repos/${REPO}/dispatches" \
   --data '{"event_type": "update_resume"}'
 }
